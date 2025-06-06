fc0682c23b57c99da78b3b9967ebdc143b61eee0
Dinesh <dineshjangra0062@gmail.com>
2025-06-06 18:59:12 +0530

    speicial for alpha

---
+++ DINESH/new.py
@@ -0,0 +1,24 @@
+# start 
+num1=int(input("enter first number:"))
+num2=int(input("enter your second number:"))
+choice=(input("enter your choice:"))
+final= choice.lower()
+
+if final=="a":
+    print("sum")
+    
+    print(num1+num2)
+elif final=="s":
+    print("subtract")
+    
+    print(num1-num2)
+elif final=="m":
+    print("multiply")
+    
+    print(num1*num2)
+elif final=="d":
+    print("division")
+    
+    print(num1/num2)
+else:
+    print("invalid")
