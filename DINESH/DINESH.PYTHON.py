efe9b2b1b9fecaeb595062a2c5d42dab519c15f5
Dinesh <dineshjangra0062@gmail.com>
2025-06-09 16:51:59 +0530

    student report

---
+++ DINESH/DINESH.PYTHON.py
@@ -0,0 +1,55 @@
+print("""
+         WELCOME TO RESULT SOFTWARE
+     This is a totally user-friendly software.
+         OK, Let's enjoy!
+""")
+
+id = int(input("Enter the student ID: "))
+name = input("Enter the student name: ")
+namef=name.capitalize()
+contact = input("Enter the student contact: ")
+address = input("Enter the student address: ")
+addressf=address.capitalize()
+
+
+print("\nOK, ENTER THE STUDENT MARKS.")
+print()
+hindi = int(input("HINDI MARKS: "))
+eng = int(input("ENGLISH MARKS: "))
+math = int(input("MATH MARKS: "))
+sci = int(input("SCIENCE MARKS: "))
+
+
+
+TOTAL = hindi + eng + math + sci
+percentage = (TOTAL / 400) * 100  
+
+if percentage>=35:
+    status="PASS."
+else:
+    status="fail !"
+
+
+
+print("#"*100)
+
+print("""                                   ...............student report....................                                                           """)
+print(f""" 
+                                                        ID       : {id}
+                                                        NAME     : {namef}
+                                                        CONTACT  : {contact}
+                                                        ADDRESS  : {addressf}
+""")
+print("."*100)
+print(f"                                                 {namef} MARKS ")
+
+print(f"""
+    HINDI    : {hindi}
+    ENGLISH  : {eng}
+    MATH     : {math}
+    SCIENCE  : {sci}
+    TOTAL    : {TOTAL}
+    PERCENT  : {percentage}%
+    STATUS   : {status}
+""")
+print("#"*100)
